3526cc0dd4710baadc2445238bf69d99aaf33f7a
Sebastian Paas <s.paas@digipen.edu>
2025-12-22 15:42:55 +0100

    updated three js version

--- src/SkyBox/OceanSkybox.ts
+++ src/SkyBox/OceanSkybox.ts
@@ -1,18 +1,9 @@
-import {
-  type Camera,
-  DoubleSide,
-  Mesh,
-  PlaneGeometry,
-  ShaderMaterial,
-  UniformsUtils,
-  Vector2,
-  Vector3,
-} from 'three';
+import * as THREE from 'three';
 import { globals } from 'Utility/global';
 import fragmentShader from './Shaders/OceanSkyBox.frag?raw';
 import vertexShader from './Shaders/OceanSkyBox.vert?raw';
 
-export class OceanSkyBox extends Mesh {
+export class OceanSkyBox extends THREE.Mesh {
 
   /**
    * Constructs a new oceandome.
@@ -21,29 +12,28 @@ export class OceanSkyBox extends Mesh {
 
     const shader = OceanSkyBox.#Shader;
 
-    const material = new ShaderMaterial({
+    const material = new THREE.ShaderMaterial({
       name: shader.name,
-      uniforms: UniformsUtils.clone(shader.uniforms),
+      uniforms: THREE.UniformsUtils.clone(shader.uniforms),
       vertexShader: shader.vertexShader,
       fragmentShader: shader.fragmentShader,
-      side: DoubleSide,
+      side: THREE.DoubleSide,
       depthWrite: true,
     });
 
-    super(new PlaneGeometry(), material);
-
+    super(new THREE.PlaneGeometry(), material);
     this.frustumCulled = false;
 
     this.onBeforeRender = (
       _renderer,
       _scene,
-      camera: Camera,
+      camera: THREE.Camera,
       //geometry: BufferGeometry,
       //material: Material,
       //group: Group
     ) => {
       material.uniforms.iTime.value = globals.time;
-      material.uniforms.iResolution.value = new Vector2(_renderer.domElement.width, _renderer.domElement.height);
+      material.uniforms.iResolution.value = new THREE.Vector2(_renderer.domElement.width, _renderer.domElement.height);
       
       camera.getWorldPosition(material.uniforms.iCameraPosition.value);
       camera.getWorldDirection(material.uniforms.iCameraForward.value);
@@ -56,11 +46,11 @@ export class OceanSkyBox extends Mesh {
     uniforms: {
       iDeepSeeDepth: { value: 10 },
       iTime: { value: 0 },
-      iSunDirection: { value: new Vector3(0.3, 1.0, 0.2).normalize() },
-      iResolution: { value: new Vector2(1920, 1080) },
-      iCameraPosition: { value: new Vector3(0, 0, 0) },
-      iCameraForward: { value: new Vector3(0, 0, -1) },
-      iCameraUp: { value: new Vector3(0, 1, 0) }
+      iSunDirection: { value: new THREE.Vector3(0.3, 1.0, 0.2).normalize() },
+      iResolution: { value: new THREE.Vector2(1920, 1080) },
+      iCameraPosition: { value: new THREE.Vector3(0, 0, 0) },
+      iCameraForward: { value: new THREE.Vector3(0, 0, -1) },
+      iCameraUp: { value: new THREE.Vector3(0, 1, 0) }
     },
     vertexShader,
     fragmentShader,
