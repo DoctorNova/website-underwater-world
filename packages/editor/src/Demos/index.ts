c2d9250f7de9f4853b5472d862651db1a1dc186d
Sebastian Paas <s.paas@digipen.edu>
2025-12-31 15:19:31 +0100

    added game as demo to editor

--- packages/editor/src/Demos/index.ts
+++ packages/editor/src/Demos/index.ts
@@ -1,6 +1,7 @@
 import type {SceneRoot} from "@engine/Composition/SceneRoot.ts";
 import {CreateShadowDemoScene, UpdateShadowDemoScene} from "./ShadowDemo.ts";
 import {CreateTestDemo, UpdateTestDemoScene} from "./TestDemo.ts";
+import {CreateGameScene} from "@game/CreateGameScene.ts";
 
 export interface Demo {
     Create: (scene: SceneRoot) => void;
@@ -9,6 +10,9 @@ export interface Demo {
 
 export const demos: Demo[] = [
     {
+        Create: CreateGameScene,
+        Update: () => {},
+    },{
         Create: CreateShadowDemoScene,
         Update: UpdateShadowDemoScene,
     },{
