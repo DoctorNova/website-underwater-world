ec8b4da93e0953ba6f995a1bc2a725973ed05f62
Sebastian Paas <s.paas@digipen.edu>
2026-01-05 12:56:43 +0100

    made background transparent again

--- packages/game/src/App/Layout/Background.tsx
+++ packages/game/src/App/Layout/Background.tsx
@@ -3,7 +3,7 @@ import {cn} from "@game/App/utils.ts";
 
 export function Background({className}: {className?: string | false}) {
     return (
-        <div className={cn("absolute inset-0 bg-linear-to-br from-[#0A1929] via-[#0d2438] to-[#132f4c] z-1", className)}>
+        <div className={cn("absolute inset-0 bg-linear-to-br from-[#0A1929]/80 via-[#0d2438]/80 to-[#132f4c]/70 z-1", className)}>
             <div className="absolute top-0 left-0 w-full h-full">
                 <div className="absolute top-1/4 left-1/4 w-96 h-96 bg-blue-500/20 rounded-full blur-3xl"></div>
                 <div className="absolute bottom-1/4 right-1/4 w-96 h-96 bg-cyan-500/20 rounded-full blur-3xl"></div>
